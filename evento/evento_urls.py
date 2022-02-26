3141686b22c42b8a1b9dc9bdd6ff76d0766ed97c
juanpabloch <63684218+juanpabloch@users.noreply.github.com>
2022-02-25 23:24:44 -0300

    index page urls

---
+++ evento/evento_urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('lista_registrados/', views.lista_personas, name='listado'),
+    # path('login/', views.login_user, name='login'),
+    # path('logout/', views.logout_user, name='logout'),
+    # path('registro/', views.registro_acceso_lista, name='registro_acceso_lista')
+]
