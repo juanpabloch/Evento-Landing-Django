c037e8883100fd4c3c439e7e5aa89b902f6929e5
juanpabloch <63684218+juanpabloch@users.noreply.github.com>
2022-02-25 23:57:18 -0300

    urls faltantes

--- evento/evento_urls.py
+++ evento/evento_urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('lista_registrados/', views.lista_personas, name='listado'),
-    # path('login/', views.login_user, name='login'),
-    # path('logout/', views.logout_user, name='logout'),
-    # path('registro/', views.registro_acceso_lista, name='registro_acceso_lista')
+    path('lista_registrados/', views.lista_personas, name='listado'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('registro/', views.registro_acceso_lista, name='registro_acceso_lista')
 ]
